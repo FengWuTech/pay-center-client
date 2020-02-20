c246406b4b2563e4acd0e8a6534e38831ab90794
codingdogs <codingdogs@163.com>
2020-02-20 10:37:10 +0800

    增加账户操作

--- api.go
+++ api.go
@@ -10,8 +10,12 @@ import (
 )
 
 const (
+	URL_CASH_PREPAY    = "http://pms-api.gmtech.top/pay-center/minip-user/recharge/cash/pay"
 	URL_RECHARGE_GOPAY = "http://pms-api.gmtech.top/pay-center/minip-user/recharge/gopay"
 	URL_BILL_GOPAY     = "http://pms-api.gmtech.top/pay-center/minip-user/billpay/gopay"
+	URL_ACCOUNT_CREATE = "http://pms-api.gmtech.top/pay-center/minip-user/account/add"
+	URL_ACCOUNT_UPDATE = "http://pms-api.gmtech.top/pay-center/minip-user/account/update"
+	URL_ACCOUNT_GET    = "http://pms-api.gmtech.top/pay-center/minip-user/account/get"
 )
 
 type PayClient struct {
@@ -30,6 +34,7 @@ func NewPayClient(appID string, apiKey string) *PayClient {
 // 充值
 func (client *PayClient) RechargeGoPay(request RechargeGoPayRequest) *RechargeGoPayResponse {
 	var sendBody, _ = json.Marshal(request)
+
 	sign := signutil.NewSign(client.ApiKey)
 	sign.AddQuery("appid", client.AppID)
 	sign.SetBody(string(sendBody))
@@ -44,6 +49,7 @@ func (client *PayClient) RechargeGoPay(request RechargeGoPayRequest) *RechargeGo
 // 账单支付
 func (client *PayClient) BillGoPay(request BillGoPayRequest) *BillGoPayResponse {
 	var sendBody, _ = json.Marshal(request)
+
 	sign := signutil.NewSign(client.ApiKey)
 	sign.AddQuery("appid", client.AppID)
 	sign.SetBody(string(sendBody))
@@ -73,4 +79,59 @@ func (client *PayClient) PayNotify(httpReq *http.Request) (error, *NotifyRequest
 	} else {
 		return errors.New("签名不一致"), nil
 	}
+}
+
+func (client *PayClient) CashRecharge(request CashRechargeRequest) *CashRechargeResponse {
+	var sendBody, _ = json.Marshal(request)
+
+	sign := signutil.NewSign(client.ApiKey)
+	sign.AddQuery("appid", client.AppID)
+	sign.SetBody(string(sendBody))
+	url := sign.GenSignURL(URL_CASH_PREPAY)
+
+	var response CashRechargeResponse
+	_, respBody := httputil.PostRawJson(url, string(sendBody))
+	json.Unmarshal(respBody, &response)
+	return &response
+}
+
+func (client *PayClient) CreateAccount(request CreateAccountRequest) *CreateAccountResponse {
+	var sendBody, _ = json.Marshal(request)
+
+	sign := signutil.NewSign(client.ApiKey)
+	sign.AddQuery("appid", client.AppID)
+	sign.SetBody(string(sendBody))
+	url := sign.GenSignURL(URL_ACCOUNT_CREATE)
+
+	var response CreateAccountResponse
+	_, respBody := httputil.PostRawJson(url, string(sendBody))
+	json.Unmarshal(respBody, &response)
+	return &response
+}
+
+func (client *PayClient) EditAccount(request EditAccountRequest) *EditAccountResponse {
+	var sendBody, _ = json.Marshal(request)
+
+	sign := signutil.NewSign(client.ApiKey)
+	sign.AddQuery("appid", client.AppID)
+	sign.SetBody(string(sendBody))
+	url := sign.GenSignURL(URL_ACCOUNT_UPDATE)
+
+	var response EditAccountResponse
+	_, respBody := httputil.PostRawJson(url, string(sendBody))
+	json.Unmarshal(respBody, &response)
+	return &response
+}
+
+func (client *PayClient) GetAccount(id int) *GetAccountResponse {
+
+	sign := signutil.NewSign(client.ApiKey)
+	sign.AddQuery("appid", client.AppID)
+	sign.AddQuery("id", id)
+	url := sign.GenSignURL(URL_ACCOUNT_GET)
+
+	var response GetAccountResponse
+	_, respBody := httputil.Get(url)
+	json.Unmarshal(respBody, &response)
+	return &response
 }
--- model.go
+++ model.go
@@ -1,7 +1,8 @@
 package pay_center_client
 
+import "time"
+
 type RechargeGoPayRequest struct {
-	PayAppID     string `json:"pay_app_id"`
 	WxSubAppID   string `json:"wx_sub_app_id"`
 	WxSubMchID   string `json:"wx_sub_mch_id"`
 	WxSubOpenID  string `json:"wx_sub_open_id"`
@@ -34,7 +35,6 @@ type BillGoPayBillListItem struct {
 }
 
 type BillGoPayRequest struct {
-	PayAppID     string                  `json:"pay_app_id"`
 	WxSubAppID   string                  `json:"wx_sub_app_id"`
 	WxSubMchID   string                  `json:"wx_sub_mch_id"`
 	WxSubOpenID  string                  `json:"wx_sub_open_id"`
@@ -69,4 +69,62 @@ type NotifyRequest struct {
 type NotifyResponse struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
+}
+
+type CashRechargeRequest struct {
+	Amount       int `json:"amount"`
+	UserID       int `json:"user_id"`
+	CompanyID    int `json:"company_id"`
+	DeductAmount int `json:"deduct_amount"`
+}
+
+type CashRechargeResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+type CreateAccountRequest struct {
+	AppID     string `json:"appid" valid:"Required"`
+	CompanyID int    `json:"company_id" valid:"Required"`
+	UserID    int    `json:"user_id" valid:"Required"`
+	Type      int    `json:"type" valid:"Required"`
+	Name      string `json:"name" valid:"Required"`
+}
+
+type CreateAccountResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data struct {
+		AccountID int `json:"account_id"`
+	} `json:"data"`
+}
+
+type EditAccountRequest struct {
+	ID     int    `json:"id" valid:"Required"`
+	Name   string `json:"name"`
+	Status int    `json:"status"`
+}
+
+type EditAccountResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+type GetAccountResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data struct {
+		ID              int       `json:"id"`
+		Appid           string    `json:"appid"`
+		CompanyId       int       `json:"company_id"`
+		UserId          int       `json:"user_id"`
+		Type            int       `json:"type"`
+		Name            string    `json:"name"`
+		Status          int       `json:"status"`
+		AmountAll       int       `json:"amount_all"`
+		AmountAvailable int       `json:"amount_available"`
+		AmountBlocked   int       `json:"amount_blocked"`
+		CreateTime      time.Time `json:"create_time"`
+		UpdateTime      time.Time `json:"update_time"`
+	} `json:"data"`
 }
