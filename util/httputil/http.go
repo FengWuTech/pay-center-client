13bf8487357690470cfa02440aabd24f3b43e0b5
codingdogs <codingdogs@163.com>
2020-02-24 11:33:34 +0800

    增加pay-center请求日志

--- util/httputil/http.go
+++ util/httputil/http.go
@@ -10,21 +10,23 @@ import (
 func Get(url string) (error, []byte) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("http请求失败， error: %v", err)
+		fmt.Printf("pay-center-client http请求失败， error: %v", err)
 		return err, nil
 	}
 
 	resBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("pay-center-client url[%v] res[%v]", url, string(resBody))
 	return nil, resBody
 }
 
 func PostRawJson(url string, body string) (error, []byte) {
 	resp, err := http.Post(url, "application/json", strings.NewReader(body))
 	if err != nil {
-		fmt.Printf("http请求失败， error: %v", err)
+		fmt.Printf("pay-center-client http请求失败， error: %v", err)
 		return err, nil
 	}
 
 	resBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("pay-center-client url[%v] body[%v] res[%v]", url, body, string(resBody))
 	return nil, resBody
 }
