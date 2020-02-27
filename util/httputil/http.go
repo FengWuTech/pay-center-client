1107108f71240a4e2964fe9c2347fa8d62b30ee5
codingdogs <codingdogs@163.com>
2020-02-27 20:28:11 +0800

    添加日志

--- util/httputil/http.go
+++ util/httputil/http.go
@@ -9,29 +9,29 @@ import (
 )
 
 func Get(url string) (error, []byte) {
+	var start = time.Now().UnixNano()
 	resp, err := http.Get(url)
 	if err != nil {
 		logutil.WarnF("url[%v] error[%v]", url, err)
 		return err, nil
 	}
-
-	var start = time.Now().UnixNano()
 	resBody, _ := ioutil.ReadAll(resp.Body)
 	var end = time.Now().UnixNano()
-	logutil.InfoF("%d url[%v] res[%v]", end-start, url, string(resBody))
+	spend := (end - start) / int64(time.Millisecond)
+	logutil.InfoF("%dms url[%v] res[%v]", spend, url, string(resBody))
 	return nil, resBody
 }
 
 func PostRawJson(url string, body string) (error, []byte) {
+	var start = time.Now().UnixNano()
 	resp, err := http.Post(url, "application/json", strings.NewReader(body))
 	if err != nil {
 		logutil.WarnF("url[%v] body[%v] error[%v]", url, body, err)
 		return err, nil
 	}
-
-	var start = time.Now().UnixNano()
 	resBody, err := ioutil.ReadAll(resp.Body)
 	var end = time.Now().UnixNano()
-	logutil.InfoF("%d url[%v] body[%v] res[%v] error[%v]", end-start, url, body, string(resBody), err)
+	spend := (end - start) / int64(time.Millisecond)
+	logutil.InfoF("%dms url[%v] body[%v] res[%v] error[%v]", spend, url, body, string(resBody), err)
 	return nil, resBody
 }
